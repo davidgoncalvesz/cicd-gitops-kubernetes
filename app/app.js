8e62c13961ad221fbff1ea577d166e33f629694a
davidgoncalvesz <82853724+davidgoncalvesz@users.noreply.github.com>
2024-01-12 00:47:06 +0100

    JASJDSKASDASKD

--- app/app.js
+++ app/app.js
@@ -12,5 +12,5 @@ app.get('/health', (req, res) => {
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Good morning, ${name}!`);
+  res.send(`FUCK THIS SHIT AAAAAAAA, ${name}!`);
 });
