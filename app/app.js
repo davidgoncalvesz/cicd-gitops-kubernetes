19b7a1d1de127f3dbb13d1db7aee07d831514ca2
davidgoncalvesz <82853724+davidgoncalvesz@users.noreply.github.com>
2024-01-12 01:10:58 +0100

    only love

--- app/app.js
+++ app/app.js
@@ -12,5 +12,5 @@ app.get('/health', (req, res) => {
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`FUCK THIS SHIT AAAAAAAA, ${name}!`);
+  res.send(`i love this <3, ${name}!`);
 });
